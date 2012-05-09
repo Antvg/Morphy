edfe42e29a52c2fc6ca5224e023f63a66b830e31
Anton Grishin <antvg@antvg.ru>
2012-05-10 00:38:59 +0400

    XML

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Morphy
 {
@@ -14,6 +15,54 @@ namespace Morphy
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlWriter output = null; //черт его знает зачем, но нужно.
+            XmlWriterSettings Settings = new XmlWriterSettings(); //XML Обьява
+
+            Settings.Indent = true; //отступ
+            Settings.IndentChars = "    "; // собственно сам отступ - 4 пробела
+
+            Settings.NewLineChars = "\n"; //переход
+
+            string txt1;
+            txt1 = textBox1.Text;
+
+            string[] Words = new string[100];
+            string[] Lex = new string[] {" ",".",",","!","?",":",";"};
+            string Word;
+            int a,b,i,j,c;
+
+            a = txt1.Length;
+            b = 1;
+
+            for (i = 1; i <= a; i++)
+            {
+                if (txt1[i] in Lex) 
+                {
+                    Word = Word + txt1[i];
+                }
+                else
+                {
+                    Words[b] = Word;
+                    Word = "";
+                    b++;
+                }
+            }
+
+            output = XmlWriter.Create("xmlt.xml", Settings);
+            
+                output.WriteStartElement(txt1);
+                output.WriteAttributeString("падеж", "склонительный");
+                output.WriteEndElement();
+                output.Flush();
+                output.Close();
+
+            
+
+
         }
     }
 }
